00a863005a9ce4a5b7d9ee79bd309d7a3d1533f5
Matecskad <darkmatee09@protonmail.com>
2026-08-02 03:26:31 +0200

    Reworked, all cars

---
+++ 2020 Toyota GR Supra A90/20supra/vehicle_names.lua
@@ -0,0 +1,4 @@
+Citizen.CreateThread(function()
+AddTextEntry("MANUFACTURER", "TOYOTA")
+AddTextEntry("CARNAME", "Supra GR Launch Edition '20")
+end)
---
+++ Debadged PD cars/DLDebadgedPD10/fxmanifest.lua
@@ -0,0 +1,16 @@
+-- DL Debadged 15 Police Car Pack Resource File
+-- https://digitiallatvia.com/
+
+fx_version 'adamant'
+game 'gta5'
+description 'Debadged 15 Police Pack'
+author 'DigitalLatvia'
+ 
+files {
+    'data/**/*.meta'
+}
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/vehiclelayouts.meta'
+data_file 'HANDLING_FILE' 'data/**/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/**/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
