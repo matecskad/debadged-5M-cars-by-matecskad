6e11058eacc92b6ea81d04aff67355f5ca26e234
Matecskad <darkmatee09@protonmail.com>
2026-08-04 21:41:04 +0200

    Few things added

---
+++ -- Sounds --/hv-enginesound/fxmanifest.lua
@@ -0,0 +1,53 @@
+fx_version 'adamant'
+game 'gta5'
+
+
+
+
+files {
+  'audioconfig/*.dat151.rel',
+  'audioconfig/*.dat54.rel',
+  "audioconfig/*.dat10.rel",
+  'sfx/**/*.awc',
+  'sfx/**/*.awc',
+  
+  
+}
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/b58b30_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/b58b30_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_b58b30'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/tascmustanggt50_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/tascmustanggt50_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_tascmustanggt50'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/chevroletlt4_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/chevroletlt4_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_chevroletlt4'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/r35sound_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/r35sound_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_r35sound'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/ta183lt1_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/ta183lt1_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_ta183lt1'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/hemisound_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/hemisound_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_hemisound'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/npolchar_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/npolchar_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_npolchar'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/ecoboostv6_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/ecoboostv6_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_ecoboostv6'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/cvpiv8_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/cvpiv8_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_cvpiv8'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/fordvoodoo_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/fordvoodoo_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_fordvoodoo'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/argento_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/argento_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_argento'
+data_file "AUDIO_SYNTHDATA" "audioconfig/lg81hcredeye_amp.dat"
+data_file "AUDIO_GAMEDATA" "audioconfig/lg81hcredeye_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/lg81hcredeye_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_lg81hcredeye"
